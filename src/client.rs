ab4180e1c2477de1c2249e28f0146266507a38cc
alexlekrow <akac92@gmail.com>
2020-08-04 00:22:50 -0700

    Moved client code to src

---
+++ src/client.rs
@@ -0,0 +1,23 @@
+use std::env;
+
+use tode::tode_client::TodeClient;
+use tode::GetHealthRequest;
+
+pub mod hello_world {
+    tonic::include_proto!("tode");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(HelloRequest {
+        name: "Tonic".into(),
+    });
+
+    let response = client.say_hello(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
+}
