fd5940a663a345854ba18d66d06be134487216cd
alexlekrow <akac92@gmail.com>
2020-08-04 20:23:44 -0700

    🔊 Add tracing to the server node

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,59 @@
 use std::env;
-
 use tonic::{transport::Server, Request, Response, Status};
-
-use tode::tode_server::{Tode, TodeServer};
-use tode::{GetHealthRequest, GetHealthResponse};
 
 pub mod tode {
     tonic::include_proto!("tode");
 }
+
+use tode::{
+    tode_server::{Tode, TodeServer},
+    GetHealthRequest, GetHealthResponse,
+};
 
 #[derive(Debug, Default)]
 pub struct TonicNode {}
 
 #[tonic::async_trait]
 impl Tode for TonicNode {
+    #[tracing::instrument]
     async fn get_health(
         &self,
         request: Request<GetHealthRequest>,
     ) -> Result<Response<GetHealthResponse>, Status> {
-        println!("Got a request: {:?}", request);
+        // tracing::info!("GetHealth: request={:?}", request);
 
-        let reply = tode::GetHealthResponse {
+        let response = Response::new(tode::GetHealthResponse {
             is_healthy: true,
             conditions: vec![
-                tode::Condition{
+                tode::Condition {
                     info: "Entered Safe condition".to_string(),
                 },
-                tode::Condition{
+                tode::Condition {
                     info: "Entered Unsafe condition".to_string(),
-                }
-            ]
-        };
-
-        Ok(Response::new(reply))
+                },
+            ],
+        });
+        tracing::info!("GetHealth: response={:?}", response);
+        Ok(response)
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Running this inside a container requires we serve on 0.0.0.0 not localhost
-    let grpc_addr = "[::0]:".to_string() + &env::var("TODE_PORT").unwrap_or("50051".to_string());
-    let tonic_node = TonicNode::default();
-    
-    println!("Serving Tonic gRPC server on {}", grpc_addr);
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .init();
+
+    // NOTE: Running this inside a container requires we serve on 0.0.0.0 not localhost
+    let addr = "[::0]:".to_string() + &env::var("TODE_PORT").unwrap_or("50051".to_string());
+    let tode = TonicNode::default();
+
+    tracing::info!(message ="Starting gRPC server.", %addr);
     Server::builder()
-        .add_service(TodeServer::new(tonic_node))
-        .serve(grpc_addr.parse()?)
+        .trace_fn(|_| tracing::info_span!("tode_server"))
+        .add_service(TodeServer::new(tode))
+        .serve(addr.parse()?)
         .await?;
-    println!("Exiting Tonic gRPC server on {}", grpc_addr);
-    
+    tracing::info!(message ="Exiting gRPC server.", %addr);
     Ok(())
 }
